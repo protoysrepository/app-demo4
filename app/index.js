de7155663226c716b9791d7465414c1c39d1e9fc
protoysrepository <153153237+protoysrepository@users.noreply.github.com>
2025-08-06 12:08:28 +0700

    Update response message

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('✅ Hello from app-demo4 via GitHub!');
+  res.send('🧪 UPDATED version via GitHub!');
 });
 
 app.listen(PORT, () => {
